df0e24079a5cd3160245eb6240769fa6a10f874b
egonw <egonw@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2008-11-21 15:38:06 +0000

    Gave it the JChemPaint title

--- src/main/org/openscience/jchempaint/application/JChemPaint.java
+++ src/main/org/openscience/jchempaint/application/JChemPaint.java
@@ -29,7 +29,7 @@ public class JChemPaint {
 	}
 	
 	public static void main(String[] args) {
-		JFrame f = new JFrame("TestApp");
+		JFrame f = new JFrame("JChemPaint");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		
