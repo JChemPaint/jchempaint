12c791f48a48b8ad4879a21ccaf1b087cc2b1f52
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2009-01-14 13:21:00 +0000

    activated the flip action

---
+++ src/main/org/openscience/jchempaint/action/FlipAction.java
@@ -0,0 +1,51 @@
+/*
+ *  $RCSfile$
+ *  $Author: egonw $
+ *  $Date: 2007-01-04 17:26:00 +0000 (Thu, 04 Jan 2007) $
+ *  $Revision: 7634 $
+ *
+ *  Copyright (C) 1997-2008 Egon Willighagen, Stefan Kuhn
+ *
+ *  Contact: cdk-jchempaint@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All we ask is that proper credit is given for our work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package org.openscience.jchempaint.action;
+
+import java.awt.event.ActionEvent;
+
+/**
+ * Action to flip selected part of a structure
+ *
+ */
+public class FlipAction extends JCPAction {
+
+	private static final long serialVersionUID = 2360209016030592684L;
+
+	public void actionPerformed(ActionEvent e) {
+        logger.info("  type  ", type);
+        logger.debug("  source ", e.getSource());
+        boolean horiz = "horizontal".equals(type);
+        jcpPanel.get2DHub().flip(horiz);
+        // fire a change so that the view gets updated
+        jcpPanel.get2DHub().updateView();
+    }
+
+}
+
