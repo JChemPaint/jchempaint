28dd5fa3164dc06156d5987d06eeca4d9be196fa
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2009-01-21 13:48:01 +0000

    added comments

--- src/main/org/openscience/jchempaint/JCPMenuTextMaker.java
+++ src/main/org/openscience/jchempaint/JCPMenuTextMaker.java
@@ -1,13 +1,50 @@
+/*
+ *  $RCSfile$
+ *  $Author: egonw $
+ *  $Date: 2007-01-04 17:26:00 +0000 (Thu, 04 Jan 2007) $
+ *  $Revision: 7634 $
+ *
+ *  Copyright (C) 1997-2008 Stefan Kuhn
+ *
+ *  Contact: cdk-jchempaint@lists.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2.1
+ *  of the License, or (at your option) any later version.
+ *  All we ask is that proper credit is given for our work, which includes
+ *  - but is not limited to - adding the above copyright notice to the beginning
+ *  of your source code files, and to any copyright notice that you may distribute
+ *  with programs based on this work.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package org.openscience.jchempaint;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This class held text entries for menu items, tool tips etc., which are
+ * configured in the JCPGUI_*.properties files. They all need an entry
+ * in the entries map to be recognized by the localizatin system.
+ *
+ */
 public class JCPMenuTextMaker {
 	private static JCPMenuTextMaker instance=null;
 	private Map<String,String> entries=new HashMap<String,String>();
 	
-	public JCPMenuTextMaker(){
+	/**
+	 * The constructor. Private since class is a singleton.
+	 */
+	private JCPMenuTextMaker(){
 		entries.put("file", GT._("File"));
 		entries.put("new", GT._("New"));
 		entries.put("atomMenuTitle", GT._("Atom Popup Menu"));
@@ -146,10 +183,21 @@ public class JCPMenuTextMaker {
 		entries.put("reactionMenuTitle", GT._("Reaction Popup Menu"));
 	}
 	
+	/**
+	 * Gives the text for an item.
+	 * 
+	 * @param key The key for the text
+	 * @return The text in current language
+	 */
 	public String getText(String key){
 		return entries.get(key);
 	}
 	
+	/**
+	 * Gives an instance of JCPMenuTextMaker.
+	 * 
+	 * @return The instance
+	 */
 	public static JCPMenuTextMaker getInstance(){
 		if(instance==null)
 			instance=new JCPMenuTextMaker();
