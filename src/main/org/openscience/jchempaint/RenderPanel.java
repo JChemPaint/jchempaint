bea8af41184f129a81da8cbd2bfc6bcfd3ee3d29
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2009-02-06 09:57:15 +0000

    more status bar stuff

--- src/main/org/openscience/jchempaint/RenderPanel.java
+++ src/main/org/openscience/jchempaint/RenderPanel.java
@@ -260,7 +260,7 @@ public class RenderPanel extends JPanel implements IViewEventRelay {
 		String status = "";
 		if (position == 0) {
 			// depict editing mode
-			status = JCPMenuTextMaker.getInstance().getText(JCPMenuTextMaker.getInstance().getText(hub.getActiveDrawModule().getDrawModeString()));
+			status = JCPMenuTextMaker.getInstance().getText(hub.getActiveDrawModule().getDrawModeString());
 		} else if (position == 1) {
 			// depict bruto formula
 		    IChemModel chemModel = hub.getIChemModel();
