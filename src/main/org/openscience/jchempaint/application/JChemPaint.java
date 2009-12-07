2a6570a2a5019ce4ed72e350ee41b759293e5826
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2009-12-07 12:13:12 +0000

    new commentary

--- src/main/org/openscience/jchempaint/application/JChemPaint.java
+++ src/main/org/openscience/jchempaint/application/JChemPaint.java
@@ -154,16 +154,12 @@ public class JChemPaint {
                 formatter.printHelp("JChemPaint", options);
 
                 // now report on the -D options
-                // TODO which of these are still used?
                 System.out.println();
                 System.out
                         .println("The -D options are as follows (defaults in parathesis):");
                 System.out.println("  cdk.debugging     [true|false] (false)");
                 System.out.println("  cdk.debug.stdout  [true|false] (false)");
-                System.out.println("  devel.gui         [true|false] (false)");
-                System.out
-                        .println("  gui               [stable|experimental] (stable)");
-                System.out.println("  user.language     [DE|EN|NL|PL] (EN)");
+                System.out.println("  user.language     [ar|cs|de|en|es|hu|nb|nl|pt|ru|th] (en)");
 
                 System.exit(0);
             }
