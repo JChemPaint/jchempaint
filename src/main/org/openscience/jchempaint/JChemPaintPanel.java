45aa9fb698acc18a8610f9aeca9c8d70ca77b5e4
annulen <annulen@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2010-01-29 08:17:53 +0000

    Use "Title" property in JChemPaintPanel

--- src/main/org/openscience/jchempaint/JChemPaintPanel.java
+++ src/main/org/openscience/jchempaint/JChemPaintPanel.java
@@ -112,7 +112,8 @@ public class JChemPaintPanel extends AbstractJChemPaintPanel implements
 			announceError(e);
 		}
 		if (gui.equals("application")) {
-			setAppTitle(" - JChemPaint");
+			setAppTitle(" - "+
+            JCPPropertyHandler.getInstance().getJCPProperties().getProperty("Title"));
 		}
         init();        
     }
