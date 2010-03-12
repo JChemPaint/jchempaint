d9262a992f94661d2ce3a8ddcb6bc6a79e14eeb9
shk3 <shk3@eb4e18e3-b210-0410-a6ab-dec725e4b171>
2010-03-12 10:47:01 +0000

    Wrong order of types

--- src/main/org/openscience/jchempaint/io/JCPSaveFileFilter.java
+++ src/main/org/openscience/jchempaint/io/JCPSaveFileFilter.java
@@ -56,8 +56,8 @@ public class JCPSaveFileFilter extends JCPFileFilter {
         chooser.addChoosableFileFilter(new JCPSaveFileFilter(JCPSaveFileFilter.cdk));
         chooser.addChoosableFileFilter(new JCPFileFilter(JCPFileFilter.cml));
         chooser.addChoosableFileFilter(new JCPFileFilter(JCPFileFilter.inchi));
-        chooser.addChoosableFileFilter(new JCPFileFilter(JCPFileFilter.mol));
         chooser.addChoosableFileFilter(new JCPFileFilter(JCPFileFilter.rxn));
+        chooser.addChoosableFileFilter(new JCPFileFilter(JCPFileFilter.mol));
     }
 
     /**
