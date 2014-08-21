4dd399e8948c491ff974a0f5226c10f5f72a1c32
John May <john.wilkinsonmay@gmail.com>
2014-08-21 13:23:24 +0100

    Nested VM instance is not bundling - for the desktop applications we can use JNI. As we are building for specific platforms we can exclude some of the native binaries.

---
+++ inchi-jni/src/main/java/org/openscience/jchempaint/inchi/JNIInChIHandler.java
@@ -0,0 +1,43 @@
+package org.openscience.jchempaint.inchi;
+
+import net.sf.jniinchi.INCHI_RET;
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.inchi.InChIGenerator;
+import org.openscience.cdk.inchi.InChIGeneratorFactory;
+import org.openscience.cdk.inchi.InChIToStructure;
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+/**
+ * InChI handler that uses JNI-InChI.
+ *
+ * @author John May
+ */
+public final class JniInChIHandler implements InChIHandler {
+
+    @Override public IAtomContainer parse(InChI inchi) throws CDKException {
+
+        InChIGeneratorFactory igf = InChIGeneratorFactory.getInstance();
+        InChIToStructure its = igf.getInChIToStructure(inchi.getInChI(),
+                                                       DefaultChemObjectBuilder.getInstance());
+
+        if (its.getReturnStatus() != INCHI_RET.OKAY && its.getReturnStatus() != INCHI_RET.WARNING)
+            throw new CDKException("Could not parse InChI - " + its.getMessage());
+
+        return its.getAtomContainer();
+    }
+
+    @Override public InChI generate(IAtomContainer container) throws CDKException {
+        InChIGeneratorFactory igf = InChIGeneratorFactory.getInstance();
+        InChIGenerator its = igf.getInChIGenerator(container);
+
+        if (its.getReturnStatus() != INCHI_RET.OKAY && its.getReturnStatus() != INCHI_RET.WARNING)
+            throw new CDKException("Could not generate InChI - " + its.getMessage());
+
+        InChI inchi = new InChI();
+        inchi.setInChI(its.getInchi());
+        inchi.setKey(its.getInchiKey());
+        inchi.setAuxInfo(its.getAuxInfo());
+        return inchi;
+    }
+}
